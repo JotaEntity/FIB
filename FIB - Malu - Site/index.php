88d50e7195ef9b0b1ba78a8994980f1760a11da2
JotaEntity <60858790+JotaEntity@users.noreply.github.com>
2020-11-10 16:18:36 -0300

    Add files via upload

---
+++ FIB - Malu - Site/index.php
@@ -0,0 +1,18 @@
+<! -- João Victor Marcelino 10/11/2020 -->
+<html>
+<meta charset="utf-8" />
+    <head>
+    <link rel="stylesheet" href="style.css">
+    </head>
+
+   <div>
+
+
+
+   <?php include('meio.html'); ?>
+
+ 
+   </div>
+
+</html>
+
