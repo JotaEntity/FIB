206c5a0a6ad2cd1d244c4d4a2e2001d5c095ad3a
JotaEntity <60858790+JotaEntity@users.noreply.github.com>
2021-03-26 20:16:30 -0300

    Update Saude.java

--- poo/Saude.java
+++ poo/Saude.java
@@ -10,8 +10,20 @@ public class Saude {
         this.altura = altura;
     }
 
-    public double calcularIMC(){
-        return Double.valueOf(this.peso / (this.altura * this.altura));
+    public String calcularIMC(){
+        double value = Double.valueOf(this.peso / (this.altura * this.altura));
+        if (value >= 40 ) {
+            return "Obesidade Grave (" + value + ")";
+        } else if (value >= 30 && value <= 39.9){
+            return "Obesidade (" + value + ")";
+        } else if (value >= 25 && value <= 29.9){
+            return "Sobrepeso (" + value + ")" ;
+        } else if (value >= 18.5 && value <= 24.9){
+            return "Normal (" + value + ")";
+        } else if (value < 18.5){
+            return "Magreza (" + value + ")";
+        }
+        return "Não foi possível calcular o seu IMC.";
     }
 
     public double getAltura() {
@@ -22,6 +34,7 @@ public class Saude {
         return peso;
     }
 }
+
 //                                 Peso, Altura
 //         Saude saude = new Saude(80, 1.80);
 //         System.out.println(saude.calcularIMC());
